2acb96d4684f21cb52ee07866953aa1c07112193
Ashkan Moradi <82896968+AshkanMoradi@users.noreply.github.com>
2025-06-02 10:13:41 -0400

    Update and rename Building-Google-Map-URL.sql to Google-Map-URL.sql

--- T-SQL/Google-Map-URL.sql
+++ T-SQL/Google-Map-URL.sql
@@ -1,4 +1,4 @@
-DECLARE @date date= '2025-01-01';
+DECLARE @date date ='2025-01-01';
 WITH CustomerLocation AS
 (
 SELECT  c.CustomerID, CONVERT(date,s.CreationDate) as Request_Date, convert(time,s.CreationDate) as Request_Time,
@@ -7,16 +7,15 @@ CONCAT(ad.Latitude,',',ad.Longitude) as Concatinated_Customer_Cordinates,
 --hs.Latitude as 'Sale_Latitude', hs.Longitude as 'Sale_Longitude',
 CONCAT(hs.Latitude,',',hs.Longitude) as Concatinated_Sales_Cordinates
 --i.Latitude as 'Invoice_Latitude',i.Longitude as 'Invoice_Longitude'
-FROM [TehranDB].[SLS3].[Customer] c
--- Due to sensitivity, Datebase Name is replaced with 'DATABASE_NAME' 
-INNER JOIN [DATABASE_NAME].[SLS3].[CustomerAddress] a      ON c.CustomerID=a.CustomerRef
-INNER JOIN [DATABASE_NAME].[GNR3].[Address] ad             ON a.AddressRef=ad.AddressID
-INNER JOIN [DATABASE_NAME].[SLS3].[SaleRequest] s          ON s.CustomerRef=c.CustomerID
-INNER JOIN [DATABASE_NAME].[SLS3].[SaleRequestItem] si     ON s.SaleRequestID=si.SaleRequestRef
-INNER JOIN [DATABASE_NAME].[DSD3].[HandheldSaleRequest] hs ON hs.SaleRequestRef=s.SaleRequestID
-INNER JOIN [DATABASE_NAME].[SLS3].[OrderItem] oi           ON oi.ReferenceRef=si.SaleRequestItemID
-INNER JOIN [DATABASE_NAME].[SLS3].[Order] o                ON o.OrderID=oi.OrderRef
-INNER JOIN [DATABASE_NAME].[DSD3].[Invoice] i              ON i.OrderRef=o.OrderID
+FROM [TehranDB].[SLS3].[Customer] c 
+INNER JOIN [SLS3].[CustomerAddress] a      ON c.CustomerID=a.CustomerRef
+INNER JOIN [GNR3].[Address] ad             ON a.AddressRef=ad.AddressID
+INNER JOIN [SLS3].[SaleRequest] s          ON s.CustomerRef=c.CustomerID
+INNER JOIN [SLS3].[SaleRequestItem] si     ON s.SaleRequestID=si.SaleRequestRef
+INNER JOIN [DSD3].[HandheldSaleRequest] hs ON hs.SaleRequestRef=s.SaleRequestID
+INNER JOIN [SLS3].[OrderItem] oi           ON oi.ReferenceRef=si.SaleRequestItemID
+INNER JOIN [SLS3].[Order] o                ON o.OrderID=oi.OrderRef
+INNER JOIN [DSD3].[Invoice] i              ON i.OrderRef=o.OrderID
 WHERE s.Date > @date 
 GROUP BY c.CustomerID,s.Number,s.CreationDate,AD.Latitude,AD.Longitude,hs.Latitude,hs.Longitude,i.Latitude,i.Longitude
 )
