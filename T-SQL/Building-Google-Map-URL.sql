c2675a82eab83abceba6e0596d9be733f31e8bf9
Ashkan Moradi <82896968+AshkanMoradi@users.noreply.github.com>
2025-06-02 10:06:28 -0400

    Update Building-Google-Map-URL.sql

--- T-SQL/Building-Google-Map-URL.sql
+++ T-SQL/Building-Google-Map-URL.sql
@@ -1,12 +1,12 @@
-DECLARE @date date= ''2025-01-01'';
+DECLARE @date date= '2025-01-01';
 WITH CustomerLocation AS
 (
 SELECT  c.CustomerID, CONVERT(date,s.CreationDate) as Request_Date, convert(time,s.CreationDate) as Request_Time,
---ad.Latitude as ''Customer_Latitude'', ad.Longitude as ''Customer_Longitude'',
-CONCAT(ad.Latitude,'','',ad.Longitude) AS Concatinated_Customer_Cordinates,
---hs.Latitude as ''Sale_Latitude'', hs.Longitude as ''Sale_Longitude'',
-CONCAT(hs.Latitude,'','',hs.Longitude) AS Concatinated_Sales_Cordinates
---i.Latitude as ''Invoice_Latitude'',i.Longitude as ''Invoice_Longitude''
+--ad.Latitude as 'Customer_Latitude', ad.Longitude as 'Customer_Longitude',
+CONCAT(ad.Latitude,',',ad.Longitude) as Concatinated_Customer_Cordinates,
+--hs.Latitude as 'Sale_Latitude', hs.Longitude as 'Sale_Longitude',
+CONCAT(hs.Latitude,',',hs.Longitude) as Concatinated_Sales_Cordinates
+--i.Latitude as 'Invoice_Latitude',i.Longitude as 'Invoice_Longitude'
 FROM [TehranDB].[SLS3].[Customer] c 
 INNER JOIN [DATABASE_NAME].[SLS3].[CustomerAddress] a      ON c.CustomerID=a.CustomerRef
 INNER JOIN [DATABASE_NAME].[GNR3].[Address] ad             ON a.AddressRef=ad.AddressID
@@ -20,6 +20,6 @@ WHERE s.Date > @date
 GROUP BY c.CustomerID,s.Number,s.CreationDate,AD.Latitude,AD.Longitude,hs.Latitude,hs.Longitude,i.Latitude,i.Longitude
 )
 SELECT CL.*,
-CONCAT(''https://www.google.com/maps/dir/?api=1&origin='',CL.Concatinated_Customer_Cordinates,''&destination='',CL.Concatinated_Sales_Cordinates,''&travelmode=driving'')
+CONCAT('https://www.google.com/maps/dir/?api=1&origin=',CL.Concatinated_Customer_Cordinates,'&destination=',CL.Concatinated_Sales_Cordinates,'&travelmode=driving')
 AS CustomerToSaleGoogleMapURL
 FROM CustomerLocation CL
