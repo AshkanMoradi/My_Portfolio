fd5048ac14e57e4512522fb8872c7e7e1b7519bc
Ashkan Moradi <82896968+AshkanMoradi@users.noreply.github.com>
2025-06-02 10:00:41 -0400

    Update and rename Cusotmer To Sale Google Map URL (OpenQuery).sql to Building-Google-Map-URL-Using-OpenQuery.sql

---
+++ T-SQL/Building-Google-Map-URL-Using-OpenQuery.sql
@@ -0,0 +1,28 @@
+SELECT * FROM OPENQUERY ([DATABASE_NAME] , '
+DECLARE @date date= ''2025-01-01'';
+WITH CustomerLocation AS
+(
+SELECT  c.CustomerID, CONVERT(date,s.CreationDate) as Request_Date, convert(time,s.CreationDate) as Request_Time,
+--ad.Latitude as ''Customer_Latitude'', ad.Longitude as ''Customer_Longitude'',
+CONCAT(ad.Latitude,'','',ad.Longitude) AS Concatinated_Customer_Cordinates,
+--hs.Latitude as ''Sale_Latitude'', hs.Longitude as ''Sale_Longitude'',
+CONCAT(hs.Latitude,'','',hs.Longitude) AS Concatinated_Sales_Cordinates
+--i.Latitude as ''Invoice_Latitude'',i.Longitude as ''Invoice_Longitude''
+FROM [TehranDB].[SLS3].[Customer] c 
+INNER JOIN [DATABASE_NAME].[SLS3].[CustomerAddress] a on c.CustomerID=a.CustomerRef
+INNER JOIN [DATABASE_NAME].[GNR3].[Address] ad on a.AddressRef=ad.AddressID
+INNER JOIN [DATABASE_NAME].[SLS3].[SaleRequest] s on s.CustomerRef=c.CustomerID
+INNER JOIN [DATABASE_NAME].[SLS3].[SaleRequestItem] si on s.SaleRequestID=si.SaleRequestRef
+INNER JOIN [DATABASE_NAME].[DSD3].[HandheldSaleRequest] hs on hs.SaleRequestRef=s.SaleRequestID
+INNER JOIN [DATABASE_NAME].[SLS3].[OrderItem] oi on oi.ReferenceRef=si.SaleRequestItemID
+INNER JOIN [DATABASE_NAME].[SLS3].[Order] o on o.OrderID=oi.OrderRef
+INNER JOIN [DATABASE_NAME].[DSD3].[Invoice] i on i.OrderRef=o.OrderID
+WHERE s.Date > @date 
+GROUP BY c.CustomerID,s.Number,s.CreationDate,AD.Latitude,AD.Longitude,hs.Latitude,hs.Longitude,i.Latitude,i.Longitude
+)
+SELECT CL.*,
+CONCAT(''https://www.google.com/maps/dir/?api=1&origin='',CL.Concatinated_Customer_Cordinates,''&destination='',CL.Concatinated_Sales_Cordinates,''&travelmode=driving'')
+AS CustomerToSaleGoogleMapURL
+FROM CustomerLocation CL
+'
+)
